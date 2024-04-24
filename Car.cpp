0b2624598fc6a21fe1e7f095a2f57709df70ac86
Bilou4000 <claire.benes@artfx.fr>
2024-04-24 20:04:13 +0200

    [wip] car collision

--- Car.cpp
+++ Car.cpp
@@ -50,72 +50,108 @@ void Car::Update(float deltaTime)
 	int tilePosY = floorf((mY / mEnvironment.mTileSize));
 	//printf( "%d %d\n", tilePosX, tilePosY);
 
-	const TileData* tiledata = mEnvironment.GetTileDataAtPos(tilePosX,tilePosY);
+	//const TileData* tiledata = mEnvironment.GetTileDataAtPos(tilePosX,tilePosY);
 
 	float speedMultiplier = 1.0f;
 
+	int carRow = floorf(mY / mEnvironment.mTileSize);
+	int carColumn = floorf(mX / mEnvironment.mTileSize);
 
 	bool hasCollided = false;
-	//for (int offsetRow = -1; offsetRow <= 1; offsetRow++)
-	//{
-	//	for (int offsetColumn = -1; offsetColumn <= 1; offsetColumn++)
-	//	{
-	//		//check if car is on same pos as brick and check the 8 around it
-	//		if (carRow + offsetRow < mEnvironment.mTilesX && carColumn + offsetColumn < mEnvironment.mTilesY
-	//			&& carRow + offsetRow >= 0 && carColumn + offsetColumn >= 0)
-	//		{
-	//			//Brick& brick = bricks[ballRow + offsetRow][ballColumn + offsetColumn];
+	for (int offsetRow = -1; offsetRow <= 1; offsetRow++)
+	{
+		for (int offsetColumn = -1; offsetColumn <= 1; offsetColumn++)
+		{
+			//check if car is on same pos as tile and check the 8 around it
+			if (carRow + offsetRow < mEnvironment.mTilesX && carColumn + offsetColumn < mEnvironment.mTilesY
+				&& carRow + offsetRow >= 0 && carColumn + offsetColumn >= 0)
+			{
+				const TileData* tiledata = mEnvironment.GetTileDataAtPos(carColumn + offsetColumn, carRow + offsetRow);
 
-	//			//if brick is already destroyed, pass
-	//			//if (brick.isDestroyed)
+				if (tiledata != nullptr)
+				{
+					if (offsetRow == 0 && offsetColumn == 0)
+					{
+						speedMultiplier = tiledata->mSpeedMultiplier;
+					}
+
+					if (tiledata->mIsObstacle)
+					{
+						float tilePosX = carColumn + offsetColumn;
+						float tilePosY = carRow + offsetRow;
+						if (CheckCollisionCircleRec({ mX, mY }, mWidth, { tilePosX * mEnvironment.mTileSize, tilePosY * mEnvironment.mTileSize }))
+						{
+							mSpeed = 0;
+						}
+						//int carrow = floorf(my / menvironment.mtilesize);
+						//int carcolumn = floorf(mx / menvironment.mtilesize);
+
+					}
+				}
+				//}
+	//			//if (tiledata != nullptr && tiledata->mIsObstacle) 
 	//			//{
-	//			//	continue;
+	//			//	mSpeed = 0;
 	//			//}
 
-	//			////if brick isn't destroy, destroy it
-	//			//if (Collision(brick.brickRec, ball.ballRec))
-	//			//{
-	//			//	ball.BounceOnBrick();
-	//			//	hasCollided = true;
-	//			//	brick.isDestroyed = true;
 
-	//			//	brickCount--;
-	//			//	score += 10;
+			}
+	////			//Brick& brick = bricks[ballRow + offsetRow][ballColumn + offsetColumn];
 
-	//			//	if (scoreThreshold <= score && life < maxLife)
-	//			//	{
-	//			//		life += 1;
-	//			//		scoreThreshold += bonusScore;
-	//			//	}
+	////			//if brick is already destroyed, pass
+	////			//if (brick.isDestroyed)
+	////			//{
+	////			//	continue;
+	////			//}
 
-	//			//	//check if every brick has been destroy
-	//			//	if (brickCount <= 0)
-	//			//	{
-	//			//		if (endOfGame)
-	//			//		{
-	//			//			EndOfGame();
-	//			//			return true;
-	//			//		}
+	////			////if brick isn't destroy, destroy it
+	////			//if (Collision(brick.brickRec, ball.ballRec))
+	////			//{
+	////			//	ball.BounceOnBrick();
+	////			//	hasCollided = true;
+	////			//	brick.isDestroyed = true;
 
-	//			//		startRowBricks += 3;
-	//			//		Init();
-	//		}
+	////			//	brickCount--;
+	////			//	score += 10;
 
-	//		break;
-	//	}
-	//}
+	////			//	if (scoreThreshold <= score && life < maxLife)
+	////			//	{
+	////			//		life += 1;
+	////			//		scoreThreshold += bonusScore;
+	////			//	}
 
-	if(tiledata != nullptr)
-	{
-		speedMultiplier = tiledata->mSpeedMultiplier;
+	////			//	//check if every brick has been destroy
+	////			//	if (brickCount <= 0)
+	////			//	{
+	////			//		if (endOfGame)
+	////			//		{
+	////			//			EndOfGame();
+	////			//			return true;
+	////			//		}
 
-		if (tiledata->mIsObstacle)
-		{
-			int carRow = floorf(mY / mEnvironment.mTileSize);
-			int carColumn = floorf(mX / mEnvironment.mTileSize);
+	////			//		startRowBricks += 3;
+	////			//		Init();
+	////		}
 
+	////		break;
 		}
 	}
+
+	//if(tiledata != nullptr)
+	//{
+	//	speedMultiplier = tiledata->mSpeedMultiplier;
+
+	//	if (tiledata->mIsObstacle)
+	//	{
+	//		//int carrow = floorf(my / menvironment.mtilesize);
+	//		//int carcolumn = floorf(mx / menvironment.mtilesize);
+	//		//mSpeed = 0;
+	//		//mX = mX - cos(mAngle) * (mSpeed * mCurrentSpeedMultiplier * deltaTime) * 5;
+	//		//mY = mY - sin(mAngle) * (mSpeed * mCurrentSpeedMultiplier * deltaTime) * 5;
+	//		//mX -= 1;
+	//		//mY -= 1;
+	//	}
+	//}
 
 	mCurrentSpeedMultiplier = Lerp( mCurrentSpeedMultiplier, speedMultiplier, deltaTime * 10.0f );
 	//printf( "%f\n", mCurrentSpeedMultiplier );
--- GameManager.cpp
+++ GameManager.cpp
@@ -9,63 +9,6 @@ void GameManager::Init()
 void GameManager::Update(float deltaTime)
 {
 	car.Update(deltaTime);
-
-    //int carRow = floorf(car.mY / environment.mTileSize);
-    //int carColumn = floorf(car.mX / environment.mTileSize);
-
-    //bool hasCollided = false;
-    //for (int offsetRow = -1; offsetRow <= 1; offsetRow++)
-    //{
-    //    for (int offsetColumn = -1; offsetColumn <= 1; offsetColumn++)
-    //    {
-    //        //check if car is on same pos as brick and check the 8 around it
-    //        if (carRow + offsetRow < environment.mTilesX && carColumn + offsetColumn < environment.mTilesY
-    //            && carRow + offsetRow >= 0 && carColumn + offsetColumn >= 0)
-    //        {
-    //            Brick& brick = bricks[ballRow + offsetRow][ballColumn + offsetColumn];
-
-    //            //if brick is already destroyed, pass
-    //            if (brick.isDestroyed)
-    //            {
-    //                continue;
-    //            }
-
-    //            //if brick isn't destroy, destroy it
-    //            if (Collision(brick.brickRec, ball.ballRec))
-    //            {
-    //                ball.BounceOnBrick();
-    //                hasCollided = true;
-    //                brick.isDestroyed = true;
-
-    //                brickCount--;
-    //                score += 10;
-
-    //                if (scoreThreshold <= score && life < maxLife)
-    //                {
-    //                    life += 1;
-    //                    scoreThreshold += bonusScore;
-    //                }
-
-    //                //check if every brick has been destroy
-    //                if (brickCount <= 0)
-    //                {
-    //                    if (endOfGame)
-    //                    {
-    //                        EndOfGame();
-    //                        return true;
-    //                    }
-
-    //                    startRowBricks += 3;
-    //                    Init();
-    //                }
-
-    //                break;
-    //            }
-    //        }
-    //    }
-
-    //    if (hasCollided) break;
-    //}
 }
 
 void GameManager::Draw()
